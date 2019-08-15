882261f04d4103fc74363c12e00aa8359956d65e
Dan Geraghty <53172896+dan-geraghty81@users.noreply.github.com>
2019-08-15 11:41:33 +1000

    Reports Controller Added

---
+++ ToolRental_BackEnd/Controllers/ReportsController.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net;
+using System.Net.Http;
+using System.Web.Http;
+using ToolRental_BackEnd.Models;
+using ToolRental_BackEnd.ViewModels;
+
+namespace ToolRental_BackEnd.Controllers
+{
+    public class ReportsController : ApiController
+    {
+		private ToolRentalEntities db = new ToolRentalEntities();
+
+		[HttpGet]
+		[Route("api/Reports/GetToolRentalCountReport")]
+		public IEnumerable<ToolRentalCountViewModel> GetToolRentalCountReport()
+		{
+			string sql = "SELECT Tools.ToolID, Tools.ToolName, COUNT(RentalItem.ToolID) AS RentalCount "
+						+"FROM Tools "
+						+"INNER JOIN RentalItem ON Tools.ToolID = RentalItem.ToolID "
+						+"GROUP BY Tools.ToolName, Tools.ToolID";
+			return db.Database.SqlQuery<ToolRentalCountViewModel>(sql).ToList();
+		}
+    }
+}
---
+++ ToolRental_BackEnd/ViewModels/ToolRentalCountViewModel.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace ToolRental_BackEnd.ViewModels
+{
+	public class ToolRentalCountViewModel
+	{
+		public int ToolID { get; set; }
+		public string ToolName { get; set; }
+		public int? RentalCount { get; set; }
+	}
+}
